e3ff005e7c5f10103b903e8498c24bf7caee6234
caiommdev <caiomotamarinho22@gmail.com>
2025-10-27 18:59:22 -0300

    first commit

---
+++ src/main/java/org/example/cardshop/CardShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.cardshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/cardshop/CardShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cardshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
